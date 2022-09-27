30ff6174f5987c8c372fa25aa70184a373c81f99
Tengyu Sun <tengyu.sun@kcl.ac.uk>
2022-09-27 15:08:35 +0100

    frequencies1

--- frequencies.py
+++ frequencies.py
@@ -4,6 +4,6 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    for key in frequencies:
+    for key in items:
         frequencies[key] = frequencies.get(key, 0) + 1
     return frequencies
