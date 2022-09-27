0ceb44f8f8b2c5e9f7c3d125fdc81f4623a47a95
Tengyu Sun <tengyu.sun@kcl.ac.uk>
2022-09-27 15:15:05 +0100

    frequencies2

--- frequencies.py
+++ frequencies.py
@@ -5,5 +5,8 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for key in items:
-        frequencies[key] = frequencies.get(key, 0) + 1
+        frequencies[str(key)] = frequencies.get(str(key), 0) + 1
     return frequencies
+
+demo = [1,22,22,33,33,1,'1']
+print(frequencies(demo))
